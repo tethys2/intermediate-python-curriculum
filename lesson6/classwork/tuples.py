79032f82a6dd9e86239900254020a29e8a61f49e
Toby Hansen <167672561+tethys2@users.noreply.github.com>
2026-05-16 00:05:49 +0000

    tuples

---
+++ lesson6/classwork/tuples.py
@@ -0,0 +1,13 @@
+point = (3, 5)
+print(point)
+
+print("x:", point[0])
+print("y:", point[1])
+
+print("Length:", len(point))
+
+info = ("Ava", 10, "Seattle")
+print(info)
+
+for item in info:
+    print(item)
